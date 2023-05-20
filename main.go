17686686ece77616dd2c929d05745eb59670a11a
Ivan Bullejos Esteban <ivan@MacBook-Pro-de-Ivan.local>
2023-05-20 23:59:55 +0200

    ejercicio 1

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
+	"github.com/PUPC-git/goBot/ejercicios"
 	"github.com/PUPC-git/goBot/variables"
 )
 
@@ -13,4 +15,27 @@ func main() {
 	fmt.Println("Pinto VariableGlobal ", variables.VariableGlobal)
 	fmt.Println("Estado: ", estado)
 	fmt.Println("Texto: ", texto)
+	//os := runtime.GOOS
+	//if os == "Linux." {
+	if os := runtime.GOOS; os == "linux" {
+		fmt.Println("Linux")
+	} else if os == "OS X" {
+		fmt.Println(os)
+	} else {
+		fmt.Println("Esto no es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
+	numero, texto := ejercicios.Ejercicio1ConvNum("233")
+
+	fmt.Println("numero: ", numero)
+	fmt.Println("texto: ", texto)
 }
